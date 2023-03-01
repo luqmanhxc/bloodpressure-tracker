18705b99e02acac8d375efe12d7a7edda30adb7e
Luqman Hakim <luqmanhxc@gmail.com>
2023-03-02 00:02:17 +0800

    feat: bp category

--- frontend/src/components/BpCard.jsx
+++ frontend/src/components/BpCard.jsx
@@ -1,10 +1,14 @@
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { checkBp } from '../utils/utils';
 
 const bpCard = ({ bp: { createdAt, systolic, diastolic, pulserate } }) => {
-    // console.log(props);
     return (
-        <Paper elevation={3} sx={{ marginTop: 3, padding: 3 }}>
+        <Paper
+            elevation={3}
+            sx={{ marginTop: 3, padding: 3, position: 'relative' }}
+        >
             <Typography variant="overline" gutterBottom>
                 {createdAt}
             </Typography>
@@ -18,9 +22,16 @@ const bpCard = ({ bp: { createdAt, systolic, diastolic, pulserate } }) => {
                 <Typography component="span"> (diastolic)</Typography>:{' '}
                 {diastolic}
             </Typography>
-            <Typography variant="h6" component="h2">
+            <Typography variant="h6" component="h2" gutterBottom>
                 Pulse Rate: {pulserate}
             </Typography>
+            <Typography variant="h5" component="h3">
+                Your blood pressure is {checkBp(systolic, diastolic)}
+            </Typography>
+            <DeleteIcon
+                color="error"
+                sx={{ position: 'absolute', top: 20, right: 20 }}
+            />
         </Paper>
     );
 };
