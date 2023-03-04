5072c1f7285e73d1573f72e4360ea5d595ce32c0
Luqman Hakim <luqmanhxc@gmail.com>
2023-03-04 16:27:05 +0800

    fix: bp message

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,13 +1,13 @@
 export const checkBp = (s, d) => {
     if (s < 120 && d < 80) {
-        return 'normal';
+        return 'Normal';
     } else if (s >= 120 && s <= 129 && d < 80) {
-        return 'elevated';
+        return 'Elevated blood pressure';
     } else if ((s >= 130 && s <= 139) || (d >= 80 && d <= 89)) {
-        return 'high blood pressure (hypertension) stage 1';
+        return 'High blood pressure (hypertension) stage 1';
     } else if (s >= 140 || d >= 90) {
-        return 'high blood pressure (hypertension) stage 2';
+        return 'High blood pressure (hypertension) stage 2';
     } else {
-        return 'hypertensive crisis';
+        return 'Hypertensive crisis';
     }
 };
